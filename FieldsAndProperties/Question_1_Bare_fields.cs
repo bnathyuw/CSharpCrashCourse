72292ca309cecebb0caa4129c396577249dd0a09
Matthew Butt <matthew@matthewbutt.com>
2012-09-25 19:18:55 +0100

    Started on fields and properties

---
+++ FieldsAndProperties/Question_1_Bare_fields.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FieldsAndProperties
+{
+    [TestFixture]
+    public class Question_1_Bare_fields
+    {
+        // TODO: What happens when you uncomment the code?
+
+        [Test]
+        public void Check_field_values()
+        {
+            var jack = new Person("Jack", 30);
+            //jack.Name = "Jill";
+            //jack.Age = 50;
+            Assert.IsTrue(jack.Name == "Jack");
+            Assert.IsTrue(jack.Age == 30);
+        }
+
+        private class Person
+        {
+            public string Name;
+            public int Age;
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+    }
+}
---
+++ FieldsAndProperties/Question_2_Properties_with_backing_fields.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FieldsAndProperties
+{
+    [TestFixture]
+    public class Question_2_Properties_with_backing_fields
+    {
+        // TODO: What happens when you uncomment the code?
+
+        [Test]
+        public void Check_field_values()
+        {
+            var jack = new Person("Jack", 30);
+            //jack.Name = "Jill";
+            //jack.Age = 50;
+            Assert.IsTrue(jack.Name == "Jack");
+            Assert.IsTrue(jack.Age == 30);
+        }
+
+        private class Person
+        {
+            private string _name;
+            private int _age;
+
+            public Person(string name, int age)
+            {
+                _name = name;
+                _age = age;
+            }
+
+            public string Name
+            {
+                get { return _name; }
+            }
+
+            public int Age
+            {
+                get { return _age; }
+            }
+        }
+    }
+}

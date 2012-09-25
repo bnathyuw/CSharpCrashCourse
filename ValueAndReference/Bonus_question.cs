0b6512b33843c276fb5f9358f2bcd3039fcc4417
Matthew Butt <matthew@matthewbutt.com>
2012-09-25 17:42:49 +0100

    Added a bonus question.

---
+++ ValueAndReference/Bonus_question.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ValueAndReference
+{
+    [TestFixture]
+    public class Bonus_question
+    {
+        private readonly List<Widget> _widgetRepository = new List<Widget>();
+
+        [Test]
+        public void When_I_process_a_widget_request_a_corresponding_widget_is_saved()
+        {
+            var widgetRequest = new WidgetRequest(3.99m);
+            var machine = new Machine(_widgetRepository);
+
+            machine.Process(widgetRequest);
+
+            var savedWidget = _widgetRepository.First();
+
+            // TODO: Why is this a useless assertion?
+            Assert.IsTrue(savedWidget.Price == widgetRequest.Price);
+        }
+    }
+
+    internal class Widget
+    {
+        public decimal Price { get; set; }
+    }
+
+    internal class Machine
+    {
+        private readonly List<Widget> _widgetRepository;
+
+        public Machine(List<Widget> widgetRepository)
+        {
+            _widgetRepository = widgetRepository;
+        }
+
+        public void Process(WidgetRequest widgetRequest)
+        {
+            const decimal arbitraryPrice = 99.99m;
+            widgetRequest.Price = arbitraryPrice;
+            var widget = new Widget {Price = arbitraryPrice};
+            _widgetRepository.Add(widget);
+        }
+    }
+
+    internal class WidgetRequest
+    {
+        public WidgetRequest(decimal price)
+        {
+            Price = price;
+        }
+
+        public decimal Price { get; set; }
+    }
+}

2a269a48835bcfd5c9fa06ed94bcc5d52ac567fe
Matthew Butt <matthew@matthewbutt.com>
2012-09-25 16:54:08 +0100

    Wrote tests for object arguments

---
+++ ValueAndReference/When_you_pass_in_an_object.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+namespace ValueAndReference
+{
+    [TestFixture]
+    public class When_you_pass_in_an_object
+    {
+        private const int ReplaceMe = 0;
+
+        [Test]
+        public void As_an_unqualified_argument()
+        {
+            var argument = new Foo(5);
+
+            ActOnArgument(argument);
+
+            Assert.IsTrue(argument.Bar == ReplaceMe);
+        }
+
+        private static void ActOnArgument(Foo argument)
+        {
+            argument.Bar++;
+        }
+
+        [Test]
+        public void As_a_reference_argument()
+        {
+            var argument = new Foo(5);
+
+            ActOnReferenceArgument(ref argument);
+
+            Assert.IsTrue(argument.Bar == ReplaceMe);
+        }
+
+        private static void ActOnReferenceArgument(ref Foo argument)
+        {
+            argument.Bar++;
+        }
+
+        [Test]
+        public void As_an_output_argument()
+        {
+            Foo argument;
+
+            ActOnOutputArgument(out argument);
+
+            Assert.IsTrue(argument.Bar == ReplaceMe);
+        }
+
+        private static void ActOnOutputArgument(out Foo argument)
+        {
+            argument = new Foo(5);
+            argument.Bar++;
+        }
+
+        private class Foo
+        {
+            public int Bar { get; set; }
+
+            public Foo(int bar)
+            {
+                Bar = bar;
+            }
+        }
+    }
+}

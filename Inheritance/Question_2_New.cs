bc0aec51affc1a9355df75d2db7fbb235547764f
Matthew Butt <matthew.butt@7digital.com>
2012-09-26 15:10:31 +0100

    Questions on inheritance

---
+++ Inheritance/Question_2_New.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+namespace Inheritance
+{
+	[TestFixture]
+	public class Question_2_New
+	{
+		private static readonly string ReplaceMe = string.Empty;
+
+		[Test]
+		public void Method_on_base_class()
+		{
+			var person = new Person();
+
+			var greeting = person.GreetMe();
+
+			// TODO: fill in the expected value
+			Assert.IsTrue(greeting == ReplaceMe);
+		}
+
+		[Test]
+		public void Override_method()
+		{
+			var enemy = new Enemy();
+
+			var greeting = enemy.GreetMe();
+
+			// TODO: fill in the expected value
+			Assert.IsTrue(greeting == ReplaceMe);
+		}
+
+		[Test]
+		public void Overridden_method_on_base_class() {
+			Person enemy = new Enemy();
+
+			var greeting = enemy.GreetMe();
+
+			// TODO: fill in the expected value
+			Assert.IsTrue(greeting == ReplaceMe);
+		}
+
+		private class Person
+		{
+			public string GreetMe()
+			{
+				return "Hello!";
+			}
+		}
+
+		private class Enemy:Person
+		{
+			public new string GreetMe()
+			{
+				return "Bugger off and leave me alone!";
+			}
+		}
+	}
+}
---
+++ Inheritance/Question_3_Inheritance.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace Inheritance
+{
+	[TestFixture]
+	public class Question_3_Inheritance
+	{
+		public static readonly string ReplaceMe = string.Empty;
+
+		// TODO: Discuss why you might want to do this
+
+		[Test]
+		public void On_class()
+		{
+			var person = new Person();
+
+			var greeting = person.GreetMe();
+
+			// TODO: fill in the expected value
+			Assert.IsTrue(greeting == ReplaceMe);
+		}
+
+		[Test]
+		public void On_interface()
+		{
+			IFriend friend = new Person();
+
+			var greeting = friend.GreetMe();
+
+			// TODO: fill in the expected value
+			Assert.IsTrue(greeting == ReplaceMe);
+		}
+
+		private class Person:IFriend
+		{
+			public string GreetMe()
+			{
+				return "Hello!";
+			}
+
+			string IFriend.GreetMe()
+			{
+				return "Hello! How lovely to see you!";
+			}
+		}
+
+		private interface IFriend
+		{
+			string GreetMe();
+		}
+	}
+}

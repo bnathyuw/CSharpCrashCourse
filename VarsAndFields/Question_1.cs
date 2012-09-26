e0733878b837e3e187f1a91a0f94616a75630583
Matthew Butt <matthew.butt@7digital.com>
2012-09-26 17:58:23 +0100

    Began on Vars and Fields

---
+++ VarsAndFields/Question_1.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace VarsAndFields
+{
+	[TestFixture]
+	public class Question_1
+	{
+		private Foo foo = new Foo();
+		private static readonly string ReplaceMe = string.Empty;
+
+		[SetUp]
+		public void SetUp()
+		{
+			//Foo.A = "Z";
+			//foo.B = "Y";
+			//foo.C = "X";
+		}
+		
+		[Test]
+		public void Static_field()
+		{
+			Assert.IsTrue(Foo.A == ReplaceMe);
+		}
+
+		[Test]
+		public void TestMethodName()
+		{
+			
+		}
+
+		[Test]
+		public void TestMethodName()
+		{
+		}
+
+		private class Foo
+		{
+			public static string A = "A";
+			public const string B = "B";
+			public readonly string C = "C";
+		}
+	}
+}

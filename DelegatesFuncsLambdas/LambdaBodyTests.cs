44d9dd9569e6212f81ca202204c18cc09348fedd
Matthew Butt <matthew@matthewbutt.com>
2012-10-16 14:05:16 +0100

    Lambda body tests.

---
+++ DelegatesFuncsLambdas/LambdaBodyTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace DelegatesFuncsLambdas
+{
+    [TestFixture]
+    public class LambdaBodyTests
+    {
+        [Test]
+        public void Action_body()
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            Action<string> hello = input =>
+                                       {
+                                           var message = "Hello, " + input + "!";
+                                           Console.Write(message);
+                                       };
+
+            hello("world");
+
+            Assert.That(stringWriter.ToString(), Is.EqualTo("Hello, world!"));
+        }
+
+        [Test]
+        public void Action_without_body()
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            Action<string> greetings = input => Console.Write("Greetings, " + input + "!");
+
+            greetings("earthlings");
+
+            Assert.That(stringWriter.ToString(), Is.EqualTo("Greetings, earthlings!"));
+        }
+
+        [Test]
+        public void Func_body()
+        {
+            Func<string, string> goodbye = input =>
+                                               {
+                                                   var message = "Goodbye, " + input + "!";
+                                                   return message;
+                                               };
+
+            Assert.That(goodbye("cruel world"), Is.EqualTo("Goodbye, cruel world!"));
+        }
+
+        [Test]
+        public void Func_without_body()
+        {
+            Func<string, string> farewell = input => "Farewell, " + input + "!";
+
+            Assert.That(farewell("Spanish ladies"), Is.EqualTo("Farewell, Spanish ladies!"));
+        }
+    }
+}
